306f68a46ed28aad23893783f7db7f38c7c7bd67
Solum <m.khang1311@gmail.com>
2026-05-18 21:32:44 -0500

    123

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const video = document.querySelector('video');
 const btn = document.getElementById('video-btn');
-
+const buttons = document.querySelectorAll('.tab-btn');
+const contents = document.querySelectorAll('.tab-content');
 const leftBar = document.querySelector('.left-bar');
 const rightBar = document.querySelector('.right-bar');
 
@@ -35,3 +36,13 @@ btn.addEventListener('click', () => {
     }
 }); 
 
+
+
+buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+        contents.forEach(content => content.classList.remove('active'));
+        contents[index].classList.add('active');
+    });
+});
